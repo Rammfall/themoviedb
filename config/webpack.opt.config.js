f3c8e3f9a2e396eaef95784154fed18bb88d2efb
Bohdan Onatskyi <15bogdan17@gmail.com>
2020-12-26 03:35:32 +0200

    Remove useless configs

--- config/webpack.opt.config.js
+++
@@ -1,8 +0,0 @@
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-
-module.exports = {
-  optimization: {
-    minimize: true
-  },
-  plugins: [new OptimizeCssAssetsPlugin()]
-}
