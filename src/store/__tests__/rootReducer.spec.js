30cad4bbcfa61e02a961519a5bd8a7f669482d12
Bohdan Onatskyi <15bogdan17@gmail.com>
2021-01-10 16:45:33 +0200

    Change rootReducer

--- src/store/__tests__/rootReducer.spec.js
+++ src/store/__tests__/rootReducer.spec.js
@@ -1,7 +1,16 @@
 import rootReducer from 'Store/rootReducer'
+import { RESET_STORE } from 'Store/types'
 
-describe('check root reducer', () => {
-  it('matches snapshot', () => {
-    expect(rootReducer).toMatchSnapshot()
+describe('rootReducer', () => {
+  it('should handle RESET_STORE', () => {
+    const state = {
+      session: {
+        isLogged: true
+      }
+    }
+    const action = { type: RESET_STORE }
+    const initialState = rootReducer(undefined, {})
+
+    expect(rootReducer(state, action)).toStrictEqual(initialState)
   })
 })
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,17 @@
 import { combineReducers } from 'redux'
 
 import session from 'Store/concepts/session/reducer'
+import { RESET_STORE } from 'Store/types'
 
 const reducers = combineReducers({
   session
 })
 
-export default reducers
+export default (state, action) => {
+  switch (action.type) {
+    case RESET_STORE:
+      return reducers(undefined, action)
+    default:
+      return reducers(state, action)
+  }
+}
