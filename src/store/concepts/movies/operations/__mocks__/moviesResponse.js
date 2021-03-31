57c21ddeacec83efc668a33248f8b4b373e1aa6f
rammfall <15bogdan17@gmail.com>
2021-03-31 16:28:03 +0300

    Fix object

--- src/store/concepts/movies/operations/__mocks__/moviesResponse.js
+++ src/store/concepts/movies/operations/__mocks__/moviesResponse.js
@@ -1,49 +1,42 @@
 export default {
-  'results': [
+  results: [
     {
-      'original_title': 'Greenland',
-      'id': 524047,
-      'video': false,
-      'vote_average': 7.3,
-      'overview': 'John Garrity, his estranged wife and their young son embark on a perilous journey to find sanctuary as a planet-killing comet hurtles toward Earth. Amid terrifying accounts of cities getting levelled, the Garrity\'s experience the best and worst in humanity. As the countdown to the global apocalypse approaches zero, their incredible trek culminates in a desperate and last-minute flight to a possible safe haven.',
-      'release_date': '2020-07-29',
-      'vote_count': 2216,
-      'adult': false,
-      'backdrop_path': '/2Fk3AB8E9dYIBc2ywJkxk8BTyhc.jpg',
-      'title': 'Greenland',
-      'genre_ids': [
-        28
-      ],
-      'poster_path': '/bNo2mcvSwIvnx8K6y1euAc1TLVq.jpg',
-      'original_language': 'en',
-      'popularity': 172.827,
-      'media_type': 'movie'
+      original_title: 'Greenland',
+      id: 524047,
+      video: false,
+      vote_average: 7.3,
+      overview:
+        "John Garrity, his estranged wife and their young son embark on a perilous journey to find sanctuary as a planet-killing comet hurtles toward Earth. Amid terrifying accounts of cities getting levelled, the Garrity's experience the best and worst in humanity. As the countdown to the global apocalypse approaches zero, their incredible trek culminates in a desperate and last-minute flight to a possible safe haven.",
+      release_date: '2020-07-29',
+      vote_count: 2216,
+      adult: false,
+      backdrop_path: '/2Fk3AB8E9dYIBc2ywJkxk8BTyhc.jpg',
+      title: 'Greenland',
+      genre_ids: [28],
+      poster_path: '/bNo2mcvSwIvnx8K6y1euAc1TLVq.jpg',
+      original_language: 'en',
+      popularity: 172.827,
+      media_type: 'movie'
     },
     {
-      'overview': 'Joe Gardner is a middle school teacher with a love for jazz music. After a successful gig at the Half Note Club, he suddenly gets into an accident that separates his soul from his body and is transported to the You Seminar, a center in which souls develop and gain passions before being transported to a newborn child. Joe must enlist help from the other souls-in-training, like 22, a soul who has spent eons in the You Seminar, in order to get back to Earth.',
-      'release_date': '2020-12-25',
-      'adult': false,
-      'backdrop_path': '/kf456ZqeC45XTvo6W9pW5clYKfQ.jpg',
-      'id': 508442,
-      'genre_ids': [
-        10751,
-        16,
-        35,
-        18,
-        10402,
-        14
-      ],
-      'original_language': 'en',
-      'original_title': 'Soul',
-      'poster_path': '/hm58Jw4Lw8OIeECIq5qyPYhAeRJ.jpg',
-      'vote_count': 5209,
-      'video': false,
-      'vote_average': 8.3,
-      'title': 'Soul',
-      'popularity': 748.13,
-      'media_type': 'movie'
+      overview:
+        'Joe Gardner is a middle school teacher with a love for jazz music. After a successful gig at the Half Note Club, he suddenly gets into an accident that separates his soul from his body and is transported to the You Seminar, a center in which souls develop and gain passions before being transported to a newborn child. Joe must enlist help from the other souls-in-training, like 22, a soul who has spent eons in the You Seminar, in order to get back to Earth.',
+      release_date: '2020-12-25',
+      adult: false,
+      backdrop_path: '/kf456ZqeC45XTvo6W9pW5clYKfQ.jpg',
+      id: 508442,
+      genre_ids: [10751, 16, 35, 18, 10402, 14],
+      original_language: 'en',
+      original_title: 'Soul',
+      poster_path: '/hm58Jw4Lw8OIeECIq5qyPYhAeRJ.jpg',
+      vote_count: 5209,
+      video: false,
+      vote_average: 8.3,
+      title: 'Soul',
+      popularity: 748.13,
+      media_type: 'movie'
     }
   ],
-  'total_pages': 1000,
-  'total_results': 20000
+  total_pages: 1000,
+  total_results: 20000
 }
